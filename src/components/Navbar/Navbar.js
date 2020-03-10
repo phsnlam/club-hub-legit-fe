d603275c1676e5d3bb1b453751d5e82be6359771
Jake <phsnlam@gmail.com>
2020-03-10 04:55:04 -0400

    Making navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,72 @@
-import React from "react";
+import React from "react"
+import styles from './Navbar/Navbar.module.css'
+import SearchBar from './components'
 
-function Navbar() {
-  return <div>Navbar</div>;
+const AppBar = () => {
+  return (
+    <div className = "navBar">
+      <div1><SearchBar/></div1>
+      <div2><Dropdown/></div2>
+    </div>
+  )
 }
+
+
+class Dropdown extends Component {
+constructor(){
+ super();
+
+ this.state = {
+       displayMenu: false,
+     };
+
+  this.showDropdownMenu = this.showDropdownMenu.bind(this);
+  this.hideDropdownMenu = this.hideDropdownMenu.bind(this);
+
+};
+
+showDropdownMenu(event) {
+    event.preventDefault();
+    this.setState({ displayMenu: true }, () => {
+    document.addEventListener('click', this.hideDropdownMenu);
+    });
+  }
+
+  hideDropdownMenu() {
+    this.setState({ displayMenu: false }, () => {
+      document.removeEventListener('click', this.hideDropdownMenu);
+    });
+
+  }
+
+  render() {
+    return (
+        <div  className="dropdown">
+          {/* To get this to work with a profile picture, pull link from database
+          and replace the title "Account Options" */}
+          <button className="button" onClick={this.showDropdownMenu}>
+            Account Options
+          </button>
+
+          { this.state.displayMenu ? (
+          <ul>
+            <a href="/"><li>Home</li></a>
+            <a href="/login"><li>Login</li></a>
+            <a href="/register"><li>Register</li></a>
+            <a href="/setting"><li>Setting</li></a>
+          </ul>
+        ):
+        (
+          null
+        )
+        }
+
+       </div>
+
+    );
+  }
+}
+
+export default AppBar
 
 export default Navbar;
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './SearchBar.module.css'
+
+const SearchBar = () => {
+   return (
+        <input type="search" placeholder="Search..." className={styles.SearchBar}/>
+)}
+
+export default SearchBar
